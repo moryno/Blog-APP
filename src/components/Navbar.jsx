6694d960a1c1963144dbab26037e3d3a71e68f48
moryno <mauricenganga41@gmail.com>
2024-12-23 15:33:57 +0300

    Add navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { IoClose } from "react-icons/io5";
+import { MdMenu } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      <div className="flex items-center gap-4 text-2xl font-bold">
+        <img src="/logo.png" className="w-8 h-8" alt="logo" />
+        <span>spikelogo</span>
+      </div>
+      <div className="md:hidden">
+        <div
+          className="cursor-pointer"
+          onClick={() => setOpen((prev) => !prev)}
+        >
+          {open ? <IoClose size={22} /> : <MdMenu size={22} />}
+        </div>
+        <div
+          className={`w-full h-screen flex flex-col gap-8 font-medium text-lg items-center justify-center absolute top-16 transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <Link to={"/"}>Home</Link>
+          <Link to={"/"}>Trending</Link>
+          <Link to={"/"}>Most Popular</Link>
+          <Link to={"/"}>About</Link>
+          <Link to={"/"}>Login</Link>
+        </div>
+      </div>
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <Link to={"/"}>Home</Link>
+        <Link to={"/"}>Trending</Link>
+        <Link to={"/"}>Most Popular</Link>
+        <Link to={"/"}>About</Link>
+        <Link to={"/"}>Login</Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
