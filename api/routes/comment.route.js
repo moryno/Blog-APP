38d9161b384eb35e7409ed1bac18abf268555492
moryno <mauricenganga41@gmail.com>
2025-01-02 12:25:35 +0300

    Add new endpoint for comments

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,14 @@
 import express from "express";
+import {
+  createComment,
+  getComments,
+  deleteComment,
+} from "../controllers/comment.controller.js";
 
 const router = express.Router();
+
+router.post("/", createComment);
+router.get("/:postId", getComments);
+router.delete("/:id", deleteComment);
 
 export default router;
