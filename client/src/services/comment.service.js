8e11498608bc34cedea6008f7372a0f7c7df2db0
moryno <mauricenganga41@gmail.com>
2025-01-02 12:24:52 +0300

    add a service for creating, getting and deleting comments

---
+++ client/src/services/comment.service.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const getComments = async (postId) => {
+  const { data } = await axios.get(
+    `${import.meta.env.VITE_API_URL}/comments/${postId}`
+  );
+  return data;
+};
+
+const createComment = (comment, token) => {
+  return axios.post(`${import.meta.env.VITE_API_URL}/comments`, comment, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+const deleteComment = (id, token) => {
+  return axios.post(`${import.meta.env.VITE_API_URL}/comments/${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const commentService = {
+  getComments,
+  createComment,
+  deleteComment,
+};
