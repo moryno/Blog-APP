6cff6c3f584f466926b1d1bfc431622456ce832c
moryno <mauricenganga41@gmail.com>
2025-01-02 10:03:32 +0300

    Add react infite-scroll-component to help in fetching post infinately on scroll

--- client/src/pages/posts/components/PostList.jsx
+++ client/src/pages/posts/components/PostList.jsx
@@ -1,29 +1,46 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import PostListItem from "./PostListItem";
 import { postService } from "../../../services/post.service";
 
 const PostList = () => {
-  const { isPending, error, data } = useQuery({
+  const {
+    data,
+    error,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isFetchingNextPage,
+  } = useInfiniteQuery({
     queryKey: ["posts"],
-    queryFn: () => postService.getPosts(),
+    queryFn: ({ pageParam = 1 }) =>
+      postService.getPosts({ page: pageParam, limit: 5 }),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage, pages) =>
+      lastPage.hasMore ? pages.length + 1 : undefined,
   });
+  if (isFetching) return <p>Loading...</p>;
+  if (!isFetching && error) return "An error has occured: " + error.message;
 
-  if (isPending) return <p>Loading...</p>;
-  if (error) return "An error has occured: " + error.message;
+  const allPosts = data?.pages?.flatMap((page) => page.posts) || [];
 
-  console.log(data);
   return (
-    <section className="flex flex-col gap-12 mb-8">
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-    </section>
+    <InfiniteScroll
+      dataLength={allPosts.length}
+      next={fetchNextPage}
+      hasMore={!!hasNextPage}
+      loader={<h4>Loading more posts...</h4>}
+      endMessage={
+        <p>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
+      {allPosts.map((post) => (
+        <PostListItem key={post._id} post={post} />
+      ))}
+    </InfiniteScroll>
   );
 };
 
