805bc209ea5be786981dc918de1f40fefef72645
moryno <mauricenganga41@gmail.com>
2025-01-02 10:05:22 +0300

    Update the posts endpoint to include author in the response and add pagination

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -3,8 +3,16 @@ import Post from "../models/post.model.js";
 import User from "../models/user.model.js";
 
 export const getPosts = async (req, res) => {
-  const posts = await Post.find();
-  res.status(200).json(posts);
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 2;
+
+  const posts = await Post.find()
+    .populate("author", "username")
+    .limit(limit)
+    .skip((page - 1) * limit);
+  const totalPosts = await Post.countDocuments();
+  const hasMore = page * limit < totalPosts;
+  res.status(200).json({ posts, hasMore });
 };
 
 export const getPost = async (req, res) => {
