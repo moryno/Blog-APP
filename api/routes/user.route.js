183fe65a3017267287fe62810cf1ab353bcf09d0
moryno <mauricenganga41@gmail.com>
2025-01-03 12:18:30 +0300

    Add route to save and get user posts

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,12 @@
 import express from "express";
+import {
+  getUserSavedPost,
+  saveUserPost,
+} from "../controllers/user.controller.js";
 
 const router = express.Router();
+
+router.get("/saved", getUserSavedPost);
+router.patch("/save", saveUserPost);
 
 export default router;
