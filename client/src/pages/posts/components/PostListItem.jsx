e9228e0abf42c81dda9fdd3ce9f739034c008f03
moryno <mauricenganga41@gmail.com>
2025-01-10 06:14:51 +0300

    Add search params to allow filtering and sorting the posts

--- client/src/pages/posts/components/PostListItem.jsx
+++ client/src/pages/posts/components/PostListItem.jsx
@@ -14,14 +14,24 @@ const PostListItem = ({ post }) => {
         />
       </div>
       <div className="flex flex-col gap-4 xl:w-2/3">
-        <Link to={`${post.slug}`} className="text-4xl font-semibold">
+        <Link to={`/${post.slug}`} className="text-4xl font-semibold">
           {post.title}
         </Link>
         <div className="flex items-center gap-2 text-gray-400 text-sm">
           <span>Author</span>
-          <Link className="text-blue-800">{post.author.username}</Link>
+          <Link
+            to={`/posts?author=${post.author.username}`}
+            className="text-blue-800"
+          >
+            {post.author.username}
+          </Link>
           <span>on</span>
-          <Link className="text-blue-800 capitalize">{post.category}</Link>
+          <Link
+            to={`/posts?category=${post.category}`}
+            className="text-blue-800 capitalize"
+          >
+            {post.category}
+          </Link>
           <span>{moment(post.createdAt).fromNow()}</span>
         </div>
         <p>{post.description}</p>
