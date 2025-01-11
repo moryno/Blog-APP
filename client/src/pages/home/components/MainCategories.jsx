4f1fc9aa9f315eb81f33666175cb0f61a087d52f
moryno <mauricenganga41@gmail.com>
2025-01-11 07:01:19 +0300

    Update maincategory outlook

--- client/src/pages/home/components/MainCategories.jsx
+++ client/src/pages/home/components/MainCategories.jsx
@@ -6,38 +6,38 @@ const MainCategories = () => {
     <section className="hidden md:flex bg-white rounded-3xl xl:rounded-full p-4 shadow-lg items-center justify-center gap-8">
       <div className="flex-1 flex items-center justify-between flex-wrap">
         <Link
-          to="/posts"
-          className="bg-blue-800 text-white rounded-full px-4 py-2"
+          to="/posts?category=general"
+          className="bg-teal-700 text-white rounded-full px-4 py-2"
         >
           All Posts
         </Link>
         <Link
           to="/posts?category=web-design"
-          className="hover:bg-blue-50 rounded-full px-4 py-2"
+          className="hover:bg-teal-50 rounded-full px-4 py-2"
         >
           Web Design
         </Link>
         <Link
           to="/posts?category=development"
-          className="hover:bg-blue-50 rounded-full px-4 py-2"
+          className="hover:bg-teal-50 rounded-full px-4 py-2"
         >
           Development
         </Link>
         <Link
           to="/posts?category=databases"
-          className="hover:bg-blue-50 rounded-full px-4 py-2"
+          className="hover:bg-teal-50 rounded-full px-4 py-2"
         >
           Databases
         </Link>
         <Link
           to="/posts?category=seo"
-          className="hover:bg-blue-50 rounded-full px-4 py-2"
+          className="hover:bg-teal-50 rounded-full px-4 py-2"
         >
           Search Engines
         </Link>
         <Link
           to="/posts?category=marketing"
-          className="hover:bg-blue-50 rounded-full px-4 py-2"
+          className="hover:bg-teal-50 rounded-full px-4 py-2"
         >
           Marketing
         </Link>
