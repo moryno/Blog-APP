b8979af0656d9b9e46e015dd959b32e64ac180bc
moryno <mauricenganga41@gmail.com>
2025-01-11 07:05:43 +0300

    Update the extended configuration to include aspect ratio

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        "3xl": "1920px",
+      },
+      flex: {
+        4: "4 1 0%",
+      },
+    },
   },
   plugins: [],
 };
