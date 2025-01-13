230b442417609f09408c611e9257bb48cc1ab044
moryno <mauricenganga41@gmail.com>
2025-01-13 07:08:12 +0300

    Add middleware to authenticate user

---
+++ api/middlewares/auth.js
@@ -0,0 +1,24 @@
+import User from "../models/user.model.js";
+
+export const authenticate = async (req, res, next) => {
+  const clerkUserId = req.auth.userId;
+
+  if (!clerkUserId) return res.status(401).json("Not Authenticated!");
+
+  const user = await User.findOne({ clerkUserId });
+
+  if (!user) return res.status(404).json("User not found!");
+
+  req.userId = user._id;
+  next();
+};
+
+export const verifyAdmin = (req, res) => {
+  const clerkUserId = req.auth.userId;
+  if (!clerkUserId) return res.status(401).json("Not Authenticated!");
+
+  const role = req.auth.sessionClaims?.metadata?.role || "user";
+
+  if (role !== "admin") return res.status(401).json("Not Authorized.");
+  next();
+};
